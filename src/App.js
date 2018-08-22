875619d89f5212fdf858af3b25c57dca9c7d4029
Ryan Sam <ryansam@carols-air.attlocal.net>
2018-08-22 11:44:22 -0500

    - Added a new DateNode component for easier manipulation of the node's attributes
    - Extracted fillCalendar() out of App's componentDidMount() lifecycle and made it into a method of App
    
    - Temporarily removed the date nodes' rotateY animation
    
     Committer: Ryan Sam <ryansam.inquiries@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,21 +1,97 @@
 import React, { Component } from 'react';
 import './App.css';
+import DateNode from './DateNode.js'
 
 class App extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      leadDates: [],
+      monthDates: [],
+      postDates: [],
+      date: new Date(),
+      monthIsOffset: false,
+      monthOffset: new Date().getMonth(),
+      yearOffset: new Date().getFullYear()
+    }
+    this.leftArrowClick = this.leftArrowClick.bind(this)
+    this.rightArrowClick = this.rightArrowClick.bind(this)
+  }
+
+  fillCalendar(year,month) {
+    //const cYear = this.state.date.getFullYear()
+    //const cMonth = this.state.date.getMonth()
+    //const { leadDates, monthDates, postDates } = this.state
+    console.log("month",month);
+    if (month - 1 === 11) { 
+      month = 0; 
+      this.setState({ monthOffset: 0 })
+      console.log("month - 1 === 11, true"); 
+      console.log("month", month)
+    }
+    if (month === 13) { 
+      month = 1; 
+      this.setState({ monthOffset: 1 }) 
+      console.log("month === 13, true"); 
+      console.log("month", month)
+    }
+    let monthStart = new Date(year,month,1).getDay()
+    let yearType = false;
+    let filledNodes = 0;
+    // Check for leap year
+    (year%4 === 0) ? 
+      (year%100 === 0) ?
+        (year%400) ? yearType = true : yearType = false : 
+      yearType = true : 
+    yearType = false
+    const monthArrays = yearType ? [31,29,31,30,31,30,31,31,30,31,30,31] : [31,28,31,30,31,30,31,31,30,31,30,31]
+    if (month - 1 === -1) { 
+      month = 12; 
+      this.setState({ monthOffset: 12 }) 
+      console.log("month - 1 === -1, true"); 
+      console.log("month", month)
+    }
+    let leadDayStart = monthArrays[month-1] - monthStart + 1
+    // Loop out lead date numbers
+    let leadDatesArray = []
+    for (let i = 0; i < monthStart; i++) {
+      leadDatesArray.push(leadDayStart)
+      leadDayStart++
+      filledNodes++
+    }
+    this.setState({ leadDates: leadDatesArray })
+    if (month + 1 === 13) { 
+      month = 0;
+      this.setState({ monthOffset: 0 });
+      console.log("month + 1 === 13, true")
+      console.log("month",month)
+    }
+    // Loop out month's date numbers
+    let monthDatesArray = []
+    for (let i = 0; i < monthArrays[month]; i++) {
+      monthDatesArray.push(i+1)
+      filledNodes++
+    }
+    this.setState({ monthDates: monthDatesArray })
+    // fill the empty remaining cells in the calendar
+    let postDatesArray = []
+    let remainingNodes = 42 - filledNodes;
+    for (let i = 0; i < remainingNodes; i++) {
+      postDatesArray.push(i+1)
+    }
+    this.setState({ postDates: postDatesArray })
+  }
+
+  componentWillMount() {
+    const cYear = this.state.date.getFullYear()
+    const cMonth = this.state.date.getMonth()
+    console.log("monthOffset",this.state.monthOffset)
+    this.fillCalendar(cYear, cMonth)
   }
   componentDidMount() {
-    const date = new Date()
-    const wkDayNames = ["Sun","Mon","Tu","Wed","Th","Fri","Sat"]
-    const datesContainer = document.getElementById('display')
-    const wkDaysContainer = document.getElementById('wkDays')
-    const cYear = date.getFullYear()
-    let yearOffset = new Date().getFullYear()
-    const cMonth = date.getMonth()
-    let monthOffset = new Date().getMonth()
-    let monthIsOffset = false
     // Loop out weekday names
+    const wkDayNames = ["Sun","Mon","Tu","Wed","Th","Fri","Sat"]
+    const wkDaysContainer = document.getElementById('wkDays')
     for (let i = 0; i < 7; i++) {
       let wkDayNode = document.createElement("div");
       wkDayNode.textContent = wkDayNames[i];
@@ -23,147 +99,50 @@ class App extends Component {
       wkDayNode.setAttribute("class", "wkDayNode");
       wkDaysContainer.appendChild(wkDayNode);
     }
-    // Loop out month's date numbers
-    for (let i = 0; i < 42; i++) {
-      let dateNode = document.createElement("div");
-      dateNode.setAttribute("class", "dateNode");
-      datesContainer.appendChild(dateNode);
-    }
-    let dateNodes = document.querySelectorAll(".dateNode")
-    let dates = [];
-    function fillCalendar(year,month) {
-      console.log(month)
-      monthIsOffset = (month === cMonth && year === cYear) ? false : true
-      if (month - 1 === 11) { month = 0; monthOffset = 0; console.log("true"); console.log(month)}
-      if (month === 13) { month = 1; monthOffset = 1; console.log("true"); console.log(month)}
-      let monthStart = new Date(year,month,1).getDay()
-      let yearType = false;
-      let filledNodes = 0;
-      // Check for leap year
-      (year%4 === 0) ? 
-        (year%100 === 0) ?
-          (year%400) ? yearType = true : yearType = false : 
-        yearType = true : 
-      yearType = false
-      const monthArrays = yearType ? [31,29,31,30,31,30,31,31,30,31,30,31] : [31,28,31,30,31,30,31,31,30,31,30,31]
-      if (month - 1 === -1) { month = 12; monthOffset = 12; console.log("true"); console.log(month)}
-      let leadDayStart = monthArrays[month-1] - monthStart + 1
-      // Loop out lead date numbers
-      for (let i = 0; i < monthStart; i++) {
-        dateNodes[i].style.background = "#8ee5ff"
-        let span = document.createElement("span")
-        span.textContent = leadDayStart
-        span.setAttribute("class", "number")
-        dateNodes[i].appendChild(span);
-        //dateNodes[i].textContent = leadDayStart;
-        leadDayStart++
-        filledNodes++
-      }
-      if (month + 1 === 13) { month = 0; console.log("true"); console.log(month)}
-      // Loop out month's date numbers
-      for (let i = 0; i < monthArrays[month]; i++) {
-        dateNodes[filledNodes].style.background = "#ffffff"
-        let span = document.createElement("span")
-        span.textContent = i + 1
-        span.setAttribute("class", "number")
-        dateNodes[filledNodes].appendChild(span);
-        //dateNodes[filledNodes].textContent = i + 1;
-        if (date.getDate() === i+1 && !monthIsOffset) { 
-          dateNodes[filledNodes].style.background = "#c4c1c1"
-        };
-        filledNodes++
-      }
-      // fill the empty remaining cells in the calendar
-      let remainingNodes = 42 - filledNodes
-      for (let i = 0; i < remainingNodes; i++) {
-        dateNodes[filledNodes].style.background = "#8ee5ff"
-        let span = document.createElement("span")
-        span.textContent = i + 1
-        span.setAttribute("class", "number")
-        dateNodes[filledNodes].appendChild(span);
-        //dateNodes[filledNodes].textContent = i + 1;
-        filledNodes++
-      }
-      return dates = document.querySelectorAll(".number");
-    }
-    fillCalendar(cYear,cMonth);
-    console.log(dates)
-    // ui module
-    let rotate = 0
-    document.getElementById("right-arrow").addEventListener("click", (e) => {
-      dates[0].classList.add("rotateNumber")
-      console.log(dates)
-      rotate -= 90;
-      // Reset the calendar cells
-      (function filp(i) {
-        setTimeout(() => {
-          dateNodes[i].style.transform = "rotateY("+rotate+"deg)"
-          dateNodes[i].removeChild(dateNodes[i].firstChild)
-          if (i !==  41) {i++;filp(i)} else {
-            rotate -= 90
-            monthOffset--
-            if (monthOffset === 11) { yearOffset--; console.log("true"); console.log(yearOffset)}
-            fillCalendar(yearOffset,monthOffset);
-
-            (function filp(j) {
-              setTimeout(() => {
-                dateNodes[j].style.transform = "rotateY("+rotate+"deg)"
-                if (j !== 41) {j++;filp(j)}
-              }, 5)
-            })(0);
-          }
-        }, 5)
-      })(0);
-      // rotate -= 90
-      // // Reset the calendar cells
-      // for (let i = 0; i < 42; i++) {
-      //   dateNodes[i].style.transform = "rotateY("+rotate+"deg)"
-      //   dateNodes[i].removeChild(dateNodes[i].firstChild)
-      // }
-      // rotate -= 90
-      // monthOffset--
-      // if (monthOffset === 11) { yearOffset--; console.log("true"); console.log(yearOffset)}
-      // fillCalendar(yearOffset,monthOffset)
-      // for (let i = 0; i < 42; i++) {
-      //   dateNodes[i].style.transform = "rotateY("+rotate+"deg)"
-      // }
-    })
-    document.getElementById("left-arrow").addEventListener("click", (e) => {
-      rotate += 90;
-      // Reset the calendar cells
-      (function filp(i) {
-        setTimeout(() => {
-          dateNodes[i].style.transform = "rotateY("+rotate+"deg)"
-          dateNodes[i].removeChild(dateNodes[i].firstChild)
-          if (i !==  41) {i++;filp(i)} else {
-            rotate += 90
-            monthOffset++
-            if (monthOffset === 12) { yearOffset++; console.log("true"); console.log(yearOffset)}
-            fillCalendar(yearOffset,monthOffset);
-
-            (function filp(i) {
-              setTimeout(() => {
-                dateNodes[i].style.transform = "rotateY("+rotate+"deg)"
-                if (i !== 41) {i++;filp(i)}
-              }, 5)
-            })(0);
-          }
-        }, 5)
-      })(0);
-      // for (let i = 0; i < 42; i++) {
-      //   dateNodes[i].style.transform = "rotateY("+rotate+"deg)"
-      //   dateNodes[i].removeChild(dateNodes[i].firstChild)
-      // }
-      // rotate += 90
-      // monthOffset++
-      // if (monthOffset === 12) { yearOffset++; console.log("true"); console.log(yearOffset)}
-      // fillCalendar(yearOffset,monthOffset)
-      // for (let i = 0; i < 42; i++) {
-      //     dateNodes[i].style.transform = "rotateY("+rotate+"deg)"
-      // }
-    })
+    console.log(this.state.leadDates, this.state.monthDates, this.state.postDates)
   }
+  componentDidUpdate(prevProps, prevState) {
+    const { yearOffset, monthOffset, leadDates, monthDates, postDates } = this.state
+    if (this.state.monthOffset !== prevState.monthOffset) {
+      this.fillCalendar(yearOffset, monthOffset)
+      console.log("componentDidUpdate", leadDates, monthDates, postDates)
+      console.log("monthOffset", this.state.monthOffset)
+    }
+  }
+
+  leftArrowClick(e) {
+    const { monthOffset, yearOffset, date } = this.state
+    const cMonth = date.getMonth();
+    const cYear = date.getFullYear();
+    this.setState(( monthOffset === 0 ) ? {
+      monthOffset: 11,
+      yearOffset: yearOffset - 1
+    } : {
+      monthOffset: monthOffset - 1,
+    }, () => {
+      const { monthOffset, yearOffset } = this.state
+      this.setState({ monthIsOffset: ( monthOffset === cMonth && yearOffset === cYear ) ? false : true })
+      console.log("yearOffset", this.state.yearOffset)
+    })
+    console.log("clicked")
+  }
+  rightArrowClick(e) {
+    const { monthOffset, yearOffset, date } = this.state
+    const cMonth = date.getMonth();
+    const cYear = date.getFullYear();
+    this.setState((prevState) => { return {
+      monthOffset: monthOffset + 1,
+      yearOffset: ( monthOffset === 11 ) ? yearOffset + 1 : yearOffset,
+    }}, () => {
+      const { monthOffset, yearOffset } = this.state
+      this.setState({ monthIsOffset: ( monthOffset === cMonth && yearOffset === cYear ) ? false : true })
+      console.log("yearOffset", this.state.yearOffset)
+    })
+    console.log("clicked")
+  }
+
   render() {
+    const { leadDates, monthDates, postDates, monthIsOffset } = this.state
     return (
       <div className="App">
         <div className="calendar">
@@ -175,7 +154,7 @@ class App extends Component {
               
             </div>
             <div className="arrow-btns">
-              <div id="left-arrow">
+              <div id="left-arrow" onClick={this.rightArrowClick}>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 6.3 4" height="60px" width="60px">
                   <title>Asset 1</title>
                   <g id="Layer_2" data-name="Layer 2">
@@ -185,7 +164,7 @@ class App extends Component {
                   </g>
                 </svg>
               </div>
-              <div id="right-arrow">
+              <div id="right-arrow" onClick={this.leftArrowClick}>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 6.3 4" height="60px" width="60px">
                   <title>Asset 1</title>
                   <g id="Layer_2" data-name="Layer 2">
@@ -201,9 +180,7 @@ class App extends Component {
             <div className="d-wkDays" id="wkDays">
 
             </div>
-            <div className="d-numbers" id="display">
-              
-            </div>
+            <DateNode leadDates={leadDates} monthDates={monthDates}  postDates={postDates} monthIsOffset={monthIsOffset}/>
           </div>
         </div>
       </div>
