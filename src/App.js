49497b92498819a31c226488d4462b7bbcf6eae3
Ryan Sam <ryansam@carols-air.attlocal.net>
2018-08-22 16:09:18 -0500

    In App.js I removed unnecessary conditions and setState calls from fillCalendar(). I also tidied fillCalendar() up be removing old commented out code and no longer needed logs to the console. Instead of using three different arrays to hold the dates I used one array to hold objects of the dates with added ids and type properties making only two props being passed to the DateNodes component necessary. I fixed the arrow click events to simplify the logic that fillCalendar() was doing before. With DateNodes.js only being passed one property for the date objects, I only need one map method to form the jsx objects to be rendered. Inside the callback for the map method I used three conditions set the date nodes to their correct colors. With these new performance updates Calendar is now at version 3.1.1.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import DateNode from './DateNode.js'
+import DateNodes from './DateNodes.js'
 
 class App extends Component {
   constructor(props) {
@@ -17,22 +17,6 @@ class App extends Component {
   }
 
   fillCalendar(year,month) {
-    //const cYear = this.state.date.getFullYear()
-    //const cMonth = this.state.date.getMonth()
-    //const { leadDates, monthDates, postDates } = this.state
-    console.log("month",month);
-    if (month - 1 === 11) { 
-      month = 0; 
-      this.setState({ monthOffset: 0 })
-      console.log("month - 1 === 11, true"); 
-      console.log("month", month)
-    }
-    if (month === 13) { 
-      month = 1; 
-      this.setState({ monthOffset: 1 }) 
-      console.log("month === 13, true"); 
-      console.log("month", month)
-    }
     let monthStart = new Date(year,month,1).getDay()
     let yearType = false;
     let filledNodes = 0;
@@ -43,35 +27,25 @@ class App extends Component {
       yearType = true : 
     yearType = false
     const monthArrays = yearType ? [31,29,31,30,31,30,31,31,30,31,30,31] : [31,28,31,30,31,30,31,31,30,31,30,31]
-    if (month - 1 === -1) { 
-      month = 12; 
-      this.setState({ monthOffset: 12 }) 
-      console.log("month - 1 === -1, true"); 
-      console.log("month", month)
-    }
+    if (month === 0) { month = 12; }
     let leadDayStart = monthArrays[month-1] - monthStart + 1
     // Loop out lead date numbers
     let datesArray = []
     for (let i = 0; i < monthStart; i++) {
-      datesArray.push({date: leadDayStart, id: "leadDate" + i})
+      datesArray.push({date: leadDayStart, type: "leadDate", id: "leadDate" + i})
       leadDayStart++
       filledNodes++
     }
-    if (month + 1 === 13) { 
-      month = 0;
-      this.setState({ monthOffset: 0 });
-      console.log("month + 1 === 13, true")
-      console.log("month",month)
-    }
+    if (month === 12) { month = 0; }
     // Loop out month's date numbers
     for (let i = 0; i < monthArrays[month]; i++) {
-      datesArray.push({date: i, id: "monthDate" + i})
+      datesArray.push({date: i + 1, type: "monthDate", id: "monthDate" + i})
       filledNodes++
     }
     // fill the empty remaining cells in the calendar
     let remainingNodes = 42 - filledNodes;
     for (let i = 0; i < remainingNodes; i++) {
-      datesArray.push({date: i, id: "postDate" + i})
+      datesArray.push({date: i + 1, type: "postDate", id: "postDate" + i})
     }
     this.setState({ datesArray: datesArray })
   }
@@ -79,12 +53,11 @@ class App extends Component {
   componentWillMount() {
     const cYear = this.state.date.getFullYear()
     const cMonth = this.state.date.getMonth()
-    console.log("monthOffset",this.state.monthOffset)
     this.fillCalendar(cYear, cMonth)
   }
   componentDidMount() {
     // Loop out weekday names
-    const wkDayNames = ["Sun","Mon","Tu","Wed","Th","Fri","Sat"]
+    const wkDayNames = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"]
     const wkDaysContainer = document.getElementById('wkDays')
     for (let i = 0; i < 7; i++) {
       let wkDayNode = document.createElement("div");
@@ -93,14 +66,11 @@ class App extends Component {
       wkDayNode.setAttribute("class", "wkDayNode");
       wkDaysContainer.appendChild(wkDayNode);
     }
-    console.log(this.state.leadDates, this.state.monthDates, this.state.postDates)
   }
   componentDidUpdate(prevProps, prevState) {
-    const { yearOffset, monthOffset, leadDates, monthDates, postDates } = this.state
+    const { yearOffset, monthOffset } = this.state
     if (this.state.monthOffset !== prevState.monthOffset) {
       this.fillCalendar(yearOffset, monthOffset)
-      console.log("componentDidUpdate", leadDates, monthDates, postDates)
-      console.log("monthOffset", this.state.monthOffset)
     }
   }
 
@@ -116,23 +86,21 @@ class App extends Component {
     }, () => {
       const { monthOffset, yearOffset } = this.state
       this.setState({ monthIsOffset: ( monthOffset === cMonth && yearOffset === cYear ) ? false : true })
-      console.log("yearOffset", this.state.yearOffset)
     })
-    console.log("clicked")
   }
   rightArrowClick(e) {
     const { monthOffset, yearOffset, date } = this.state
     const cMonth = date.getMonth();
     const cYear = date.getFullYear();
-    this.setState((prevState) => { return {
-      monthOffset: monthOffset + 1,
-      yearOffset: ( monthOffset === 11 ) ? yearOffset + 1 : yearOffset,
-    }}, () => {
+    this.setState((monthOffset === 11) ? {
+      monthOffset: 0,
+      yearOffset: yearOffset + 1
+    } : {
+      monthOffset: monthOffset + 1
+    }, () => {
       const { monthOffset, yearOffset } = this.state
       this.setState({ monthIsOffset: ( monthOffset === cMonth && yearOffset === cYear ) ? false : true })
-      console.log("yearOffset", this.state.yearOffset)
     })
-    console.log("clicked")
   }
 
   render() {
@@ -148,7 +116,7 @@ class App extends Component {
               
             </div>
             <div className="arrow-btns">
-              <div id="left-arrow" onClick={this.rightArrowClick}>
+              <div id="right-arrow" onClick={this.rightArrowClick}>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 6.3 4" height="60px" width="60px">
                   <title>Asset 1</title>
                   <g id="Layer_2" data-name="Layer 2">
@@ -158,7 +126,7 @@ class App extends Component {
                   </g>
                 </svg>
               </div>
-              <div id="right-arrow" onClick={this.leftArrowClick}>
+              <div id="left-arrow" onClick={this.leftArrowClick}>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 6.3 4" height="60px" width="60px">
                   <title>Asset 1</title>
                   <g id="Layer_2" data-name="Layer 2">
@@ -174,7 +142,7 @@ class App extends Component {
             <div className="d-wkDays" id="wkDays">
 
             </div>
-            <DateNode datesArray={datesArray} monthIsOffset={monthIsOffset}/>
+            <DateNodes datesArray={datesArray} monthIsOffset={monthIsOffset}/>
           </div>
         </div>
       </div>
